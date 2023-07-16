9fd3260820a7574b4cd9a1bc0bb7e1aa74a24f8f
Vignesh <vighneshgupta32@gmail.com>
2023-07-16 19:21:41 +0530

    [added] Day 6: Data Types

---
+++ DAY6/dataType.js
@@ -0,0 +1,47 @@
+let fullName = "John Deos" // String
+let address = null // Null
+let job = undefined // Undefined
+
+
+// ########## String ########## 
+// 0 -> J, 1 -> o, 2 -> h, 3 -> n, 4 -> space, 5 -> D, 6 -> e, 7 -> o, 8 -> s
+// console.log(fullName.charAt(5));
+// console.log(fullName.charAt(fullName.length - 1));
+// console.log(fullName.indexOf('D'));
+// console.log(fullName.toLowerCase());
+// console.log(fullName.slice(5, 8));
+
+// let arr = fullName.split(' ');
+// console.log(arr);
+// console.log(arr[arr.length - 1]);
+// let isAStudent = " is a strudent";
+// let extra = " & is also a teacher";
+// console.log(fullName.concat(isAStudent, extra));
+// console.log(fullName + isAStudent + extra);
+
+// ########## Numbers ########## 
+let age = 22 // Number
+
+// console.log(age)
+// console.log(10 + 10 + age + 10+ 10); //20221010 // 62
+// console.log(age - 10) 
+// console.log(age.toString())
+// console.log(age.toPrecision(4))
+// console.log(typeof age.valueOf());
+
+
+// ########## Boolean ##########
+let isMarried = false // Boolean
+// console.log(isMarried.valueOf())
+
+
+// ########## Convert ##########
+// ########## String <-> Number ##########
+let num = 12
+// console.log(parseInt(num).toString()) // "10" -> 10 -> "10"
+// console.log(parseFloat(num))
+
+// ########## String <-> Boolean ##########
+let str = "false" 
+console.log(str === "true") 
+console.log(num >= 10) 
