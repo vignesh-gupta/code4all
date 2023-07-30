b5c19ee56daedaddc3996478a4a8c1756c1f9325
Vignesh <vighneshgupta32@gmail.com>
2023-07-30 18:51:21 +0530

    [added] params & DOM

---
+++ DAY9/script.js
@@ -0,0 +1,9 @@
+// console.log(document);
+
+// document.write("<h2>Hello World</h2>");
+let topHeading = document.getElementById("top-heading");
+topHeading.innerHTML = "Hello <br> World";
+// document.getElementById("top-heading2").innerText = "Hello <br> World2";
+// document.getElementById("top-heading3").textContent = "Hello <br> World3";
+
+topHeading.style.background = "red";
