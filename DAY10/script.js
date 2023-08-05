1ba51ae2e8ecbb654998f6ff3f9f1d897b360840
Vignesh <vighneshgupta32@gmail.com>
2023-08-05 19:19:14 +0530

    [added] DAY10 : array methods pt.II

---
+++ DAY10/script.js
@@ -0,0 +1,20 @@
+// console.time('Your code took');
+
+// let heading = document.getElementById("heading")
+// heading.style.padding = "5px";
+// heading.style.border = "7px double black";
+// heading.style.margin = "9px";
+
+// console.timeEnd('Your code took');
+
+// let bgRedElements = document.getElementsByClassName("bg-red");
+
+// for(let i = 0; i < bgRedElements.length; i++){
+//   bgRedElements[i].innerHTML = "I am changed using JS loop " + i;
+// }
+
+// let header = document.getElementsByTagName("h1");
+// for(let i = 0; i < header.length; i++){
+//   header[i].innerHTML = "I am changed using JS header loop " + i;
+// }
+
