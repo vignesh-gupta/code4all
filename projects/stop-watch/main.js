939fecb4486d8cd213187257190f60a5f4c4bd9e
Vignesh Gupta <vighneshgupta32@gmail.com>
2023-09-10 19:06:42 +0530

    [added] Stop watch - final

--- projects/stop-watch/main.js
+++ projects/stop-watch/main.js
@@ -1,8 +1,14 @@
 let time = document.getElementById("time");
+let startBtn = document.getElementById("start-btn");
+let stopBtn = document.getElementById("stop-btn");
 
 let min = 0,
   sec = 0,
   msec = 0;
+
+function formatNum(num) {
+  return num < 10 ? "0" + num.toString() : num.toString();
+}
 
 let printFormattedTime = () => {
   time.innerHTML = `${formatNum(min)} : ${formatNum(sec)} : ${formatNum(msec)}`;
@@ -11,14 +17,18 @@ let printFormattedTime = () => {
 // TO keep track of interval key
 let timerInterval;
 
-document.getElementById("start-btn").addEventListener('click', () => {
-
+startBtn.addEventListener("click", () => {
   // avoid BUG#1 -> Speed up and never stop on double click of start
-  // Clear interval if any exist to 
-  clearInterval(timerInterval) 
+  // Clear interval if any exist to
+  clearInterval(timerInterval);
 
+  // Hide Start btn
+  startBtn.classList.add("btn-hidden");
 
-  timerInterval =  setInterval(() => {
+  // Show stop btn
+  stopBtn.classList.remove("btn-hidden");
+
+  timerInterval = setInterval(() => {
     ++msec;
     if (msec > 99) {
       (msec = 0), ++sec;
@@ -28,23 +38,50 @@ document.getElementById("start-btn").addEventListener('click', () => {
     }
     printFormattedTime();
   }, 10);
+
+  console.log(timerInterval);
 });
 
-document.getElementById("stop-btn").addEventListener('click' , ()=>{
-  clearInterval(timerInterval)
-})
+stopBtn.addEventListener("click", () => {
+  clearInterval(timerInterval);
 
-document.getElementById("reset-btn").addEventListener('click', ()=>{
-  clearInterval(timerInterval)
-  min = 0,
-  sec = 0,
-  msec = 0;
+  // Hide stop btn
+  stopBtn.classList.add("btn-hidden");
+
+  // Show start btn
+  startBtn.classList.remove("btn-hidden");
+});
+
+document.getElementById("reset-btn").addEventListener("click", () => {
+  clearInterval(timerInterval);
+
+  (min = 0), (sec = 0), (msec = 0);
 
   // BUG#2 -> Numbers are not updating on click of reset
   // TO update the screen
-  printFormattedTime()
-})
+  printFormattedTime();
 
-function formatNum(num) {
-  return num < 10 ? "0" + num.toString() : num.toString();
-}
+  // Hide stop btn
+  stopBtn.classList.add("btn-hidden");
+
+  // Show start btn
+  startBtn.classList.remove("btn-hidden");
+
+  document.getElementById("laps-list").innerHTML = "";
+});
+
+// FEATURE#1 - Add lapping feature.
+
+document.getElementById("lap-btn").addEventListener("click", () => {
+  if (min === 0 && sec === 0 && msec === 0) return;
+
+  let captureTime = `${formatNum(min)} : ${formatNum(sec)} : ${formatNum(
+    msec
+  )}`;
+  console.log(captureTime);
+
+  let lapEle = document.createElement("li");
+  lapEle.innerText = captureTime;
+
+  document.getElementById("laps-list").appendChild(lapEle);
+});
